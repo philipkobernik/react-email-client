7c0368c5993bee04b52065bc2182257a6807ca7d
Philip Kobernik <ptk921@gmail.com>
2014-05-08 02:46:06 -0700

    Nav Component

--- assets/app.js
+++ assets/app.js
@@ -1,31 +1,38 @@
 /** @jsx React.DOM */
 
+var Nav = React.createClass({
+  render: function() {
+    return (
+      <div id="nav" className="pure-u">
+          <a href="#" className="nav-menu-button">Menu</a>
+
+          <div className="nav-inner">
+              <button className="primary-button pure-button">Compose</button>
+
+              <div className="pure-menu pure-menu-open">
+                  <ul>
+                      <li><a href="#">Inbox <span className="email-count">(2)</span></a></li>
+                      <li><a href="#">Important</a></li>
+                      <li><a href="#">Sent</a></li>
+                      <li><a href="#">Drafts</a></li>
+                      <li><a href="#">Trash</a></li>
+                      <li className="pure-menu-heading">Labels</li>
+                      <li><a href="#"><span className="email-label-personal"></span>Personal</a></li>
+                      <li><a href="#"><span className="email-label-work"></span>Work</a></li>
+                      <li><a href="#"><span className="email-label-travel"></span>Travel</a></li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+    );
+  }
+});
+
 var App = React.createClass({
   render: function() {
     return (
 <div id="layout" className="content pure-g">
-    <div id="nav" className="pure-u">
-        <a href="#" className="nav-menu-button">Menu</a>
-
-        <div className="nav-inner">
-            <button className="primary-button pure-button">Compose</button>
-
-            <div className="pure-menu pure-menu-open">
-                <ul>
-                    <li><a href="#">Inbox <span className="email-count">(2)</span></a></li>
-                    <li><a href="#">Important</a></li>
-                    <li><a href="#">Sent</a></li>
-                    <li><a href="#">Drafts</a></li>
-                    <li><a href="#">Trash</a></li>
-                    <li className="pure-menu-heading">Labels</li>
-                    <li><a href="#"><span className="email-label-personal"></span>Personal</a></li>
-                    <li><a href="#"><span className="email-label-work"></span>Work</a></li>
-                    <li><a href="#"><span className="email-label-travel"></span>Travel</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-
+    <Nav />
     <div id="list" className="pure-u-1">
         <div className="email-item email-item-selected pure-g">
             <div className="pure-u">

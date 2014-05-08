1f435fa0bc36bf9ce78111f71bc63d0c07b048f8
Philip Kobernik <ptk921@gmail.com>
2014-05-08 02:47:40 -0700

    JSX-ize the List component

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,110 @@
 /** @jsx React.DOM */
+var List = React.createClass({
+  render: function() {
+    return (
+      <div id="list" className="pure-u-1">
+          <div className="email-item email-item-selected pure-g">
+              <div className="pure-u">
+                  <img className="email-avatar" alt="Tilo Mitra&#x27;s avatar" height="64" width="64" src="img/common/tilo-avatar.png" />
+              </div>
+
+              <div className="pure-u-3-4">
+                  <h5 className="email-name">Tilo Mitra</h5>
+                  <h4 className="email-subject">Hello from Toronto</h4>
+                  <p className="email-desc">
+                      Hey, I just wanted to check in with you from Toronto. I got here earlier today.
+                  </p>
+              </div>
+          </div>
+
+          <div className="email-item email-item-unread pure-g">
+              <div className="pure-u">
+                  <img className="email-avatar" alt="Eric Ferraiuolo&#x27;s avatar" height="64" width="64" src="img/common/ericf-avatar.png" />
+              </div>
+
+              <div className="pure-u-3-4">
+                  <h5 className="email-name">Eric Ferraiuolo</h5>
+                  <h4 className="email-subject">Re: Pull Requests</h4>
+                  <p className="email-desc">
+                      Hey, I had some feedback for pull request #51. We should center the menu so it looks better on mobile.
+                  </p>
+              </div>
+          </div>
+
+          <div className="email-item email-item-unread pure-g">
+              <div className="pure-u">
+                  <img className="email-avatar" alt="YUI&#x27;s avatar" height="64" width="64" src="img/common/yui-avatar.png" />
+              </div>
+
+              <div className="pure-u-3-4">
+                  <h5 className="email-name">YUI Library</h5>
+                  <h4 className="email-subject">You have 5 bugs assigned to you</h4>
+                  <p className="email-desc">
+                      Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla.
+                  </p>
+              </div>
+          </div>
+
+          <div className="email-item pure-g">
+              <div className="pure-u">
+                  <img className="email-avatar" alt="Reid Burke&#x27;s avatar" height="64" width="64" src="img/common/reid-avatar.png" />
+              </div>
+
+              <div className="pure-u-3-4">
+                  <h5 className="email-name">Reid Burke</h5>
+                  <h4 className="email-subject">Re: Design Language</h4>
+                  <p className="email-desc">
+                      Excepteur sint occaecat cupidatat non proident, sunt in culpa.
+                  </p>
+              </div>
+          </div>
+
+          <div className="email-item pure-g">
+              <div className="pure-u">
+                  <img className="email-avatar" alt="Andrew Wooldridge&#x27;s avatar" height="64" width="64" src="img/common/andrew-avatar.png" />
+              </div>
+
+              <div className="pure-u-3-4">
+                  <h5 className="email-name">Andrew Wooldridge</h5>
+                  <h4 className="email-subject">YUI Blog Updates?</h4>
+                  <p className="email-desc">
+                      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+                  </p>
+              </div>
+          </div>
+
+          <div className="email-item pure-g">
+              <div className="pure-u">
+                  <img className="email-avatar" alt="Yahoo! Finance&#x27;s Avatar" height="64" width="64" src="img/common/yfinance-avatar.png" />
+              </div>
+
+              <div className="pure-u-3-4">
+                  <h5 className="email-name">Yahoo! Finance</h5>
+                  <h4 className="email-subject">How to protect your finances from winter storms</h4>
+                  <p className="email-desc">
+                      Mauris tempor mi vitae sem aliquet pharetra. Fusce in dui purus, nec malesuada mauris.
+                  </p>
+              </div>
+          </div>
+
+          <div className="email-item pure-g">
+              <div className="pure-u">
+                  <img className="email-avatar" alt="Yahoo! News&#x27; avatar" height="64" width="64" src="img/common/ynews-avatar.png" />
+
+              </div>
+
+              <div className="pure-u-3-4">
+                  <h5 className="email-name">Yahoo! News</h5>
+                  <h4 className="email-subject">Summary for April 3rd, 2012</h4>
+                  <p className="email-desc">
+                      We found 10 news articles that you may like.
+                  </p>
+              </div>
+          </div>
+      </div>
+    );
+  }
+});
 
 var Nav = React.createClass({
   render: function() {
@@ -33,106 +139,7 @@ var App = React.createClass({
     return (
 <div id="layout" className="content pure-g">
     <Nav />
-    <div id="list" className="pure-u-1">
-        <div className="email-item email-item-selected pure-g">
-            <div className="pure-u">
-                <img className="email-avatar" alt="Tilo Mitra&#x27;s avatar" height="64" width="64" src="img/common/tilo-avatar.png" />
-            </div>
-
-            <div className="pure-u-3-4">
-                <h5 className="email-name">Tilo Mitra</h5>
-                <h4 className="email-subject">Hello from Toronto</h4>
-                <p className="email-desc">
-                    Hey, I just wanted to check in with you from Toronto. I got here earlier today.
-                </p>
-            </div>
-        </div>
-
-        <div className="email-item email-item-unread pure-g">
-            <div className="pure-u">
-                <img className="email-avatar" alt="Eric Ferraiuolo&#x27;s avatar" height="64" width="64" src="img/common/ericf-avatar.png" />
-            </div>
-
-            <div className="pure-u-3-4">
-                <h5 className="email-name">Eric Ferraiuolo</h5>
-                <h4 className="email-subject">Re: Pull Requests</h4>
-                <p className="email-desc">
-                    Hey, I had some feedback for pull request #51. We should center the menu so it looks better on mobile.
-                </p>
-            </div>
-        </div>
-
-        <div className="email-item email-item-unread pure-g">
-            <div className="pure-u">
-                <img className="email-avatar" alt="YUI&#x27;s avatar" height="64" width="64" src="img/common/yui-avatar.png" />
-            </div>
-
-            <div className="pure-u-3-4">
-                <h5 className="email-name">YUI Library</h5>
-                <h4 className="email-subject">You have 5 bugs assigned to you</h4>
-                <p className="email-desc">
-                    Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla.
-                </p>
-            </div>
-        </div>
-
-        <div className="email-item pure-g">
-            <div className="pure-u">
-                <img className="email-avatar" alt="Reid Burke&#x27;s avatar" height="64" width="64" src="img/common/reid-avatar.png" />
-            </div>
-
-            <div className="pure-u-3-4">
-                <h5 className="email-name">Reid Burke</h5>
-                <h4 className="email-subject">Re: Design Language</h4>
-                <p className="email-desc">
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa.
-                </p>
-            </div>
-        </div>
-
-        <div className="email-item pure-g">
-            <div className="pure-u">
-                <img className="email-avatar" alt="Andrew Wooldridge&#x27;s avatar" height="64" width="64" src="img/common/andrew-avatar.png" />
-            </div>
-
-            <div className="pure-u-3-4">
-                <h5 className="email-name">Andrew Wooldridge</h5>
-                <h4 className="email-subject">YUI Blog Updates?</h4>
-                <p className="email-desc">
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
-                </p>
-            </div>
-        </div>
-
-        <div className="email-item pure-g">
-            <div className="pure-u">
-                <img className="email-avatar" alt="Yahoo! Finance&#x27;s Avatar" height="64" width="64" src="img/common/yfinance-avatar.png" />
-            </div>
-
-            <div className="pure-u-3-4">
-                <h5 className="email-name">Yahoo! Finance</h5>
-                <h4 className="email-subject">How to protect your finances from winter storms</h4>
-                <p className="email-desc">
-                    Mauris tempor mi vitae sem aliquet pharetra. Fusce in dui purus, nec malesuada mauris.
-                </p>
-            </div>
-        </div>
-
-        <div className="email-item pure-g">
-            <div className="pure-u">
-                <img className="email-avatar" alt="Yahoo! News&#x27; avatar" height="64" width="64" src="img/common/ynews-avatar.png" />
-
-            </div>
-
-            <div className="pure-u-3-4">
-                <h5 className="email-name">Yahoo! News</h5>
-                <h4 className="email-subject">Summary for April 3rd, 2012</h4>
-                <p className="email-desc">
-                    We found 10 news articles that you may like.
-                </p>
-            </div>
-        </div>
-    </div>
+    <List />
 
     <div id="main" className="pure-u-1">
         <div className="email-content">
